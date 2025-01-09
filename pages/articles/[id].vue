a453dace7dc17ac491ca3dc01563571b4109307b
Htin Linn Phyo <htinlinnphyo.npt@gmail.com>
2025-01-09 18:28:19 +0630

    test ssr in article detail

--- pages/articles/[id].vue
+++ pages/articles/[id].vue
@@ -24,62 +24,59 @@ watch(
     localLoading.value = true;
     // console.log("hey");
     await fetchArticleDetail({ article_id: newId });
+    useHead({
+  title: localizedArticle.value.localizedTitle || "Loading ...",
+  meta: [
+    {
+      hid: "description",
+      name: "description",
+      content: `Read this article Now.`,
+    },
+    { hid: "og:title", property: "og:title", content: localizedArticle.value.localizedTitle || "Loading ..." },
+    {
+      hid: "og:description",
+      property: "og:description",
+      content: `Read this article Now.`,
+    },
+    {
+      hid: "og:image",
+      property: "og:image",
+      content: localizedArticle.value.thumbnail || "default-thumbnail.jpg",
+    },
+    {
+      hid: "og:url",
+      property: "og:url",
+      content: `${domainName}${route.fullPath}`,
+    },
+    { hid: "og:type", property: "og:type", content: "article" },
+    { hid: "twitter:title", name: "twitter:title", content: localizedArticle.value.localizedTitle || "Default Title" },
+    {
+      hid: "twitter:description",
+      name: "twitter:description",
+      content: `Read this article Now.`,
+    },
+    {
+      hid: "twitter:image",
+      name: "twitter:image",
+      content: localizedArticle.value.thumbnail || "default-thumbnail.jpg",
+    },
+    {
+      hid: "twitter:card",
+      name: "twitter:card",
+      content: "summary_large_image",
+    },
+  ],
+});
+
     localLoading.value = false;
   },
   {
     immediate: true,
   }
 );
+console.log(localizedArticle)
 const domainName = "https://buddhist.news";
-watch(
-  localizedArticle,
-  (article) => {
-    useHead({
-      title: article.title,
-      meta: [
-        {
-          hid: "description",
-          name: "description",
-          content: `Read this article Now.`,
-        },
-        { hid: "og:title", property: "og:title", content: article.title },
-        {
-          hid: "og:description",
-          property: "og:description",
-          content: `Read this article Now.`,
-        },
-        {
-          hid: "og:image",
-          property: "og:image",
-          content: article.thumbnail,
-        },
-        {
-          hid: "og:url",
-          property: "og:url",
-          content: `${domainName}${route.fullPath}`,
-        },
-        { hid: "og:type", property: "og:type", content: "article" },
-        { hid: "twitter:title", name: "twitter:title", content: article.title },
-        {
-          hid: "twitter:description",
-          name: "twitter:description",
-          content: `Read this article Now.`,
-        },
-        {
-          hid: "twitter:image",
-          name: "twitter:image",
-          content: article.thumbnail,
-        },
-        {
-          hid: "twitter:card",
-          name: "twitter:card",
-          content: "summary_large_image",
-        },
-      ],
-    });
-  },
-  { immediate: true }
-);
+
 </script>
 
 <template>
