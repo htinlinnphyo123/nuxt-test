92f5bd48842e20890f85c32e82b66d04e1f77466
Nay Ba La <naybalaismylife.nbl@gmail.com>
2025-01-10 13:34:05 +0630

    copy : about page from old vue :<

--- components/Footer/LeftSide/LeftSide.vue
+++ components/Footer/LeftSide/LeftSide.vue
@@ -4,7 +4,7 @@
     <div class="mt-4 xl:ps-1">
       <div v-for="about in props.localizedAbout" :key="about.slug" class="my-2">
         <NuxtLink
-          to="/"
+          :to="{ name: 'about' }"
           class="hover:underline hover:text-brand-secondary text-sm"
         >
           {{ about.localizedTitle }}
---
+++ pages/about/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="min-h-screen pt-4 lg:pt-8 px-2 lg:px-8 mt-4">
+    <div v-if="loading">
+      <AboutPageLoading />
+    </div>
+    <div v-else>
+      <Content :localizedPage="localizedPage" />
+    </div>
+  </div>
+</template>
+<script setup>
+import useAboutPage from "@/composables/Home/about";
+import AboutPageLoading from "@/components/About/AboutPageLoading.vue";
+import Content from "@/components/About/Content.vue";
+import { onMounted } from "vue";
+const { localizedPage, error, fetchAbout, loading } = useAboutPage();
+onMounted(() => {
+  fetchAbout();
+});
+</script>
